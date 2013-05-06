281907c11e9282f9012adde18c8cbf1207233dba
Kier Davis <kierdavis@gmail.con>
2013-05-06 15:30:57 +0100

    Added loading/saving support

--- src/MyMOTD.java
+++ src/MyMOTD.java
@@ -4,16 +4,32 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class MyMOTD extends JavaPlugin {
     private String motd;
+    private File motdFile;
     
     public void onEnable() {
-        motd = "\247cSet an MOTD using /motd ...";
+        motd = "\247cSet a MOTD using /motd ...";
+        motdFile = new File(getDataFolder(), "motd.txt");
+        
+        if (motdFile.exists()) {
+            BufferedReader r = new BufferedReader(new InputStreamReader(new FileInputStream(motdFile), "UTF-8"));
+            motd = r.readLine();
+            r.close();
+        }
         
         getServer().getPluginManager().registerEvents(new ServerListPingListener(this), this);
         getCommand("motd").setExecutor(new MOTDCommandExecutor(this));
     }
     
     public void onDisable() {
+        if (!motdFile.getParentFile().exists()) {
+            motdFile.getParentFile().mkdirs();
+        }
         
+        BufferedWriter w = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(motdFile), "UTF-8"));
+        w.write(motd);
+        w.newLine();
+        w.flush();
+        w.close();
     }
     
     public String getMOTD() {
